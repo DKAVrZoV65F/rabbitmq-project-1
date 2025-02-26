5edd1ba850a4a533e5bd1325316b482daf65e193
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-02-26 13:50:42 +0300

    feat: Init project

---
+++ rabbitmq/src/main/java/com/nerzon/rabbitmq/service/MessageSender.java
@@ -0,0 +1,22 @@
+package com.nerzon.rabbitmq.service;
+
+import lombok.Setter;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Setter
+@Service
+public class MessageSender {
+
+    @Value("${queue.name}")
+    private String queueName;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    public void send(String message) {
+        amqpTemplate.convertAndSend(queueName, message);
+    }
+}
